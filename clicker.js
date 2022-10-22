7b0d997fe6c8fd6cc9cb441c5815d067f1d3a4bc
lux <ssbekmuratov@gmail.com>
2022-10-22 23:15:47 +0600

    js changings

--- clicker.js
+++ clicker.js
@@ -2,6 +2,17 @@ let counter = 0;
 function count() {
     counter += 1;
     document.getElementById("click").innerHTML = counter;
+    if(counter == 5){
+        document.getElementById("ButtonBar").style.background = "darkblue";
+    }else if(counter == 10){
+        document.getElementById("ButtonBar").style.background = "yellow";
+    }else if(counter == 20){
+        document.getElementById("ButtonBar").style.background = "red";
+    }else if(counter == 50){
+        document.getElementById("ButtonBar").style.background = "silver";
+    }else if(counter == 100){
+        document.getElementById("ButtonBar").style.background = "gold";
+    }
 }
 
 function reset() {
